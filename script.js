79bc5146ccfb069fce864bba330a4ff4d323be59
lojadotesoro <lojadotesourooficial@gmail.com>
2025-07-31 15:17:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.getElementById('cadastroForm').addEventListener('submit', function(event) {
-    const nome = document.getElementById('nome').value;
-    const telefone = document.getElementById('telefone').value;
-
-    // Redireciona para WhatsApp depois de 1 segundo
-    setTimeout(() => {
-        window.location.href = `https://wa.me/${telefone}?text=Olá meu nome ${nome}, estou precisando de ajuda com meu produto`;
-    }, 1000);
-});
+// Não há redirecionamento automático. Tudo manual.
