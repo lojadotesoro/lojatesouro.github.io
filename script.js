c4df4c4f2ff90692c6178ae7a3e366558fc6e2f3
lojadotesoro <lojadotesourooficial@gmail.com>
2025-07-31 15:42:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// Arquivo de scripts para futuras funcionalidades
+document.addEventListener("DOMContentLoaded", () => {
+    const params = new URLSearchParams(window.location.search);
+    const nome = params.get("nome");
+
+    const whatsappLink = document.getElementById("whatsappLink");
+    if (whatsappLink && nome) {
+        const mensagem = `Olá, meu nome é ${nome}. Acabei de me cadastrar no site e estou precisando de uma ajudinha com o meu produto. Poderiam me auxiliar?`;
+        whatsappLink.href = `https://wa.me/5516997986457?text=${encodeURIComponent(mensagem)}`;
+    }
+});
