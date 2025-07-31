3fa49f34c606fbbf45a3df7567fa227dfb386a37
lojadotesoro <lojadotesourooficial@gmail.com>
2025-07-31 18:36:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 function redirecionarFinal() {
-    // Evita redirecionamento múltiplo caso o usuário clique mais de uma vez
-    if (window.redirecionando) return;
-    window.redirecionando = true;
-
-    // Aumenta o tempo para garantir que o Google Forms processe os dados
-    setTimeout(function () {
+    setTimeout(function(){
         window.location.href = 'final.html';
-    }, 1500); // 1,5 segundos para o redirecionamento
+    }, 1500); // 1,5 segundos de espera para garantir o envio antes de redirecionar
 }
