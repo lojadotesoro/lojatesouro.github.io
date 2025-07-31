880ae1067041f61c3e910c20925d450bcefabb63
lojadotesoro <lojadotesourooficial@gmail.com>
2025-07-31 16:45:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-document.getElementById('cadastroForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    // Após enviar o cadastro, esconde o formulário e mostra o botão do WhatsApp
-    document.getElementById('cadastroForm').style.display = 'none';
-    document.getElementById('whatsappLink').classList.remove('hidden');
-});
+function redirecionarFinal() {
+    setTimeout(function(){
+        window.location.href = 'final.html';
+    }, 500); // tempo para envio antes do redirecionamento
+}
